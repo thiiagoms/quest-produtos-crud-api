fc6b74ba03ec77a41b44ef623880d234d0cacbb1
thiiagoms <thiagom.devsec@gmail.com>
2023-01-25 09:17:36 -0300

    feat: finish product crud :sparkles:

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+/**
+ * Product Controller
+ *
+ * @package App\Http\Controllers
+ * @author  Thiago Silva <thiagom.devsec@gmail.com>
+ * @version 1.0
+ */
+final class ProductController extends Controller
+{
+    /**
+     * Init controller with service
+     *
+     * @param ProductService $productService
+     */
+    public function __construct(private ProductService $productService)
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('products.index', ['products' => $this->productService->listProducts()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\ProductRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $productImg = null;
+
+        if ($request->hasFile('productImage')) {
+            $productImg = $request->file('productImage')->getClientOriginalName();
+            $request->file('productImage')->move(public_path('products'), $productImg);
+        }
+
+        $productName  = strip_tags($request->productName);
+        $productPrice = filter_var($request->productPrice, FILTER_VALIDATE_FLOAT);
+        $productDescription = strip_tags($request->productDescription);
+
+        $result = $this->productService->createProduct([
+            'name'        => $productName,
+            'price'       => $productPrice,
+            'description' => $productDescription,
+            'img_path'    => $productImg
+        ]);
+
+        return to_route('product.index')->with(array_key_first($result), array_values($result)[0]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('products.show', ['product' => $this->productService->findProduct($id)]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('products.edit', ['product' => $this->productService->findProduct($id)]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, $productId)
+    {
+        $result = $this->productService->updateProduct($productId, $request->except(['_token', '_method']));
+        return to_route('product.index')->with(array_key_first($result), array_values($result)[0]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $result = $this->productService->deleteProduct($id);
+        return to_route('product.index')->with(array_key_first($result), array_values($result)[0]);
+    }
+}
---
+++ app/Services/ProductService.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Contracts\ProductContract;
+
+/**
+ * Product Service
+ *
+ * @package App\Services
+ * @author  Thiago Silva <thiagom.devsec@gmail.com>
+ * @version 1.0
+ */
+final class ProductService
+{
+    public function __construct(private ProductContract $productContract)
+    {
+    }
+
+    /**
+     * List all products
+     *
+     * @return object
+     */
+    public function listProducts(): object
+    {
+        return $this->productContract->productList();
+    }
+
+    /**
+     * Return product data if exits or null instead
+     *
+     * @param integer $productId
+     * @return object|null
+     */
+    public function findProduct(int $productId): ?object
+    {
+        $result = $this->productContract->productFind($productId);
+        return is_null($result) ? null : $result;
+    }
+
+    /**
+     * Create new product
+     *
+     * @param array $productData
+     * @return array
+     */
+    public function createProduct(array $productData): array
+    {
+        $result = $this->productContract->productCreate($productData);
+
+        return is_integer($result) === true
+            ? ['success' => "Produto {$productData['name']} cadastrado com sucesso!!"]
+            : ['error'   => "Erro ao cadastrar o produto {$productData['name']}, tente novamente mais tarde!!"];
+    }
+
+    /**
+     * Update Product
+     *
+     * @param integer $productId
+     * @param array $data
+     * @return array
+     */
+    public function updateProduct(int $productId, array $data): array
+    {
+        $productData = [
+            'name'        => $data['productName'],
+            'price'       => $data['productPrice'],
+            'description' => $data['productDescription'],
+            'img_path'    => $data['productImage']
+        ];
+
+        $result = $this->findProduct($productId);
+
+        $result->fill($productData)->save();
+
+        return $result instanceof \App\Models\Product
+            ? ['success' => "Produto {$productData['name']} atualizado com sucesso"]
+            : ['error'   => "Falha ao atualizar o produto {$productData['name']}, tente novamente mais tarde"];
+    }
+
+    /**
+     * Delete product
+     *
+     * @param integer $productId
+     * @return array
+     */
+    public function deleteProduct(int $productId): array
+    {
+        $productData = $this->findProduct($productId);
+        $result = $this->productContract->productDestroy($productId);
+
+        return $result === true
+            ? ['success' => "Produto {$productData->name} deletado com sucesso"]
+            : ['error' => "Erro ao deletar produto"];
+    }
+}
